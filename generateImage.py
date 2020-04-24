2e32487f73fb2cebbdc5947633499f2dceda785e
shreyas <shreyasnbhat@gmail.com>
2020-04-24 03:46:38 -0700

    Added video player initial code and metadata generation code(NOT WORKING)

--- generateImage.py
+++ generateImage.py
@@ -1,61 +1,124 @@
-import pandas as pd 
+import pandas as pd
 import cv2
 import numpy as np
 from pyCAIR import cropByColumn
 import sys
 import os
 
-def genImageFromVideo(folderName, outputPath):
-	fileName = folderName.split('/')[-1]
-	df = pd.read_csv(folderName+'/'+fileName+'-Scenes.csv')
-	numberOfScenes = df.shape[0]-1
-	finalImage = []
+METADATA_FILE = "metadata.csv"
 
-	for i in range(1,numberOfScenes+1):
-		num = "{0:03}".format(i)
-		img = cv2.imread(folderName+'/'+fileName+'-Scene-'+str(num)+'-01.jpg')
-		if i==1:
-			finalImage = img
-		else:
-			finalImage = np.hstack((finalImage,img))
-	print(finalImage.shape)
-	print(outputPath)
-	cv2.imwrite(outputPath,finalImage)
+
+class VideoMetaData:
+    def __init__(self, path, frameNumber, timeStamp):
+        self.path = path
+        self.frameNumber = frameNumber
+        self.timeStamp = timeStamp
+
+    def write(self, path):
+        f = open(path, "a+")
+        content = "VID," + self.path + "," + self.frameNumber + "," + str(float(self.timeStamp) * 1000) + "\n"
+        f.write(content)
+        f.close()
+
+
+class ImageMetaData:
+    def __init__(self, path):
+        self.path = path
+
+    # Ideally a csv
+    def write(self, path):
+        f = open(path, "a+")
+        content = "IMG," + self.path + "\n"
+        f.write(content)
+        f.close()
+
+
+# Stores Metadata of First Frame of each scene
+def processVideoMetaData(metapath, videoSourcePath):
+    f = open(metapath, "r")
+    res = f.readlines()
+
+    for i in range(2, len(res)):
+        path = videoSourcePath
+        contents = res[i].split(',')
+        vidMeta = VideoMetaData(path, contents[1], contents[3])
+        vidMeta.write(METADATA_FILE)
+
+    f.close()
+
+
+def genImageFromVideo(folderName, outputPath, videoSourcePath=""):
+    fileName = folderName.split('/')[-1]
+
+    metapath = folderName + '/' + fileName + '-Scenes.csv'
+    df = pd.read_csv(metapath)
+
+    processVideoMetaData(metapath, videoSourcePath)
+
+    numberOfScenes = df.shape[0] - 1
+    finalImage = []
+
+    for i in range(1, numberOfScenes + 1):
+        num = "{0:03}".format(i)
+        imgPath = folderName + '/' + fileName + '-Scene-' + str(num) + '-01.jpg'
+
+        img = cv2.imread(imgPath)
+        if i == 1:
+            finalImage = img
+        else:
+            finalImage = np.hstack((finalImage, img))
+
+    cv2.imwrite(outputPath, finalImage)
+
+
+def isVideo(path):
+    return ("finalimage" in path)
+
 
 def mergeImages(pathImage):
-	finalImage = None
-	for filename in os.listdir(pathImage):
-		if filename.endswith(".jpg"):
-			imgPath = os.path.join(pathImage, filename)
-			img = cv2.imread(imgPath)
-			if finalImage is None:
-				finalImage = img
-			else :
-				finalImage = np.hstack((finalImage,img))
-	cv2.imwrite(pathImage+'finalImage.png',finalImage)
+    finalImage = None
+
+    for filename in os.listdir(pathImage):
+        if filename.endswith(".jpg"):
+            imgPath = os.path.join(pathImage, filename)
+
+            # Write image metadata if not a video summary
+            if not isVideo(imgPath):
+                imageMetaData = ImageMetaData(imgPath)
+                imageMetaData.write(METADATA_FILE)
+
+            img = cv2.imread(imgPath)
+            if finalImage is None:
+                finalImage = img
+            else:
+                finalImage = np.hstack((finalImage, img))
+
+    cv2.imwrite(pathImage + 'finalImage.png', finalImage)
+
 
 def reshapeFinalImage(fileName):
-	finalImage = cv2.imread(fileName)
-	height = finalImage.shape[0]
-	if finalImage.shape[1]>1080:
-		finalImage = cv2.resize(finalImage,(1080,height))
-	cv2.imwrite(fileName,finalImage)
+    finalImage = cv2.imread(fileName)
+    height = finalImage.shape[0]
+    if finalImage.shape[1] > 1080:
+        finalImage = cv2.resize(finalImage, (1080, height))
+    cv2.imwrite(fileName, finalImage)
 
-def seamcarveFinalImage(fileName):
-	finalImage = cv2.imread(fileName)
-	height = finalImage.shape[0]
-	if finalImage.shape[1]>1080:
-		ratio = 1080/finalImage.shape[1]
-		print(ratio)
-		seam_img, finalImage = cropByColumn(finalImage, 1, 0, ['trial','png'], 0.95, 0)
-	cv2.imwrite('badaimage2.png',finalImage)
+
+def seamCarveFinalImage(fileName):
+    finalImage = cv2.imread(fileName)
+    height = finalImage.shape[0]
+    if finalImage.shape[1] > 1080:
+        ratio = 1080 / finalImage.shape[1]
+        print(ratio)
+        seam_img, finalImage = cropByColumn(finalImage, 1, 0, ['trial', 'png'], 0.95, 0)
+    cv2.imwrite('badaimage2.png', finalImage)
 
 
 if __name__ == '__main__':
-	if len(sys.argv)==3:
-		genImageFromVideo(sys.argv[1],sys.argv[2])
-	elif len(sys.argv)==2:
-		genImageFromVideo(sys.argv[1])
-	else:
-		print("Please provide file path to the input video")
-	reshapeFinalImage(sys.argv[2])
+    if len(sys.argv) == 3:
+        genImageFromVideo(sys.argv[1], sys.argv[2], sys.argv[1])
+    elif len(sys.argv) == 2:
+        genImageFromVideo(sys.argv[1], "Processing", sys.argv[1])
+    else:
+        print("Please provide file path to the input video")
+    reshapeFinalImage(sys.argv[2])
--- generateImageFromFolder.py
+++ generateImageFromFolder.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 import cv2
 import numpy as np
 from pyCAIR import cropByColumn
@@ -6,45 +6,63 @@ import sys
 import os
 from scenesfromvideo import runSceneDetection
 from rgb import readRGBFromDirectory
-from generateImage import genImageFromVideo
-from generateImage import mergeImages
-from generateImage import reshapeFinalImage
+from generateImage import genImageFromVideo, mergeImages, reshapeFinalImage
 
-def genImageFromFolder(inputFolder,outputFolder='Processing/'):
-	cwd = os.getcwd()
-	path = os.path.join(cwd, outputFolder)
-	if not os.path.isdir(path):
-		os.mkdir(path)
-	rootdir = inputFolder
-	extensions = ('.avi')
-	videoFilePath = []
-	for subdir, dirs, files in os.walk(rootdir):
-		for file in files:
-			ext = os.path.splitext(file)[-1].lower()
-			if ext in extensions:
-				filePath = os.path.abspath(os.path.join(subdir, file))
-				fileName = os.path.splitext(file)[-2]
-				outputVideoPath = os.path.join(path,fileName)
-				videoFilePath.append(outputVideoPath)
-				if not os.path.isdir(outputVideoPath):
-					os.mkdir(outputVideoPath)
-				runSceneDetection(filePath, outputVideoPath)
 
-	readRGBFromDirectory(inputFolder,path+'/images')
+def genImageFromFolder(inputFolder, outputFolder='Processing'):
+    cwd = os.getcwd()
+    path = os.path.join(cwd, outputFolder)
 
-	for file in videoFilePath:
-		videoPath = path+'images/finalimage'+file.split('/')[-1]+'.jpg'
-		genImageFromVideo(file,videoPath)
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-	mergeImages(path+'/images/')
+    rootdir = inputFolder
+    extensions = ('.avi')
 
-	# reshapeFinalImage(path+'/images/finalImage.png')
+    vidSourcePaths = []
+    videoSDOutputPaths = []
+
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            ext = os.path.splitext(file)[-1].lower()
+
+            if ext in extensions:
+                filePath = os.path.abspath(os.path.join(subdir, file))
+                fileName = os.path.splitext(file)
+                outputVideoPath = os.path.join(path, fileName[-2])
+
+                # Store Video Source Paths
+                vidSourcePaths.append(filePath)
+
+                # Stores Scene Detect Video Output directory paths
+                videoSDOutputPaths.append(outputVideoPath)
+
+                if not os.path.isdir(outputVideoPath):
+                    os.mkdir(outputVideoPath)
+
+                print(filePath)
+                print(outputVideoPath)
+
+                runSceneDetection(filePath, outputVideoPath)
+
+    readRGBFromDirectory(inputFolder, path + '/images')
+
+    for file, ofile in zip(videoSDOutputPaths, vidSourcePaths):
+        videoTitle = file.split('/')[-1]
+        videoImageSummaryPath = path + '/images/finalimage' + str(videoTitle) + '.jpg'
+        print("Generating Image File for", file, videoImageSummaryPath)
+        genImageFromVideo(file, videoImageSummaryPath, ofile)
+
+    mergeImages(path + '/images/')
+
+
+# reshapeFinalImage(path+'/images/finalImage.png')
 
 
 if __name__ == '__main__':
-	if len(sys.argv)==3:
-		genImageFromFolder(sys.argv[1],sys.argv[2])
-	elif len(sys.argv)==2:
-		genImageFromFolder(sys.argv[1])
-	else:
-		print("Please provide file path to the input folder")
+    if len(sys.argv) == 3:
+        genImageFromFolder(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 2:
+        genImageFromFolder(sys.argv[1])
+    else:
+        print("Please provide file path to the input folder")
