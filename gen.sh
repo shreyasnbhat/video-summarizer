806a664f27e1d5f0d13c9b5f466169be1e5876f4
shreyas <shreyasnbhat@gmail.com>
2020-04-30 22:37:40 -0700

    Script update

--- gen.sh
+++ gen.sh
@@ -1,15 +1,24 @@
-VID=3
+MAXVID=$1
+
+echo $MAXVID
+
 rm -rvf Intermediary Results Final
 
 mkdir Intermediary
 mkdir Results
 mkdir Results/images
 
-# Just for a video
-python3 createvideo.py Data/576RGBVideo$VID Intermediary/576RGBVideo$VID Data/video_$VID.wav Results/
-python3 scenesfromvideo.py Results/576RGBVideo$VID.mp4 Intermediary/scenes
-python3 frame_extractor.py Intermediary/scenes/576RGBVideo$VID-Scenes.csv Intermediary/576RGBVideo$VID/ Results/576RGBVideo$VID/
-mv Results/576RGBVideo$VID/576RGBVideo$VID-Scenes.csv Results/576RGBVideo$VID-Scenes.csv
+python3 rgb.py Data/image Results/images
+
+for (( VID=1; VID<=$MAXVID; VID++ ))
+do
+echo "Video: video$VID"
+VIDNAME=video$VID
+python3 createvideo.py Data/$VIDNAME Intermediary/$VIDNAME Data/$VIDNAME/audio.wav Results/
+python3 scenesfromvideo.py Results/$VIDNAME.mp4 Intermediary/scenes
+python3 frame_extractor.py Intermediary/scenes/$VIDNAME-Scenes.csv Intermediary/$VIDNAME/ Results/$VIDNAME/
+mv Results/$VIDNAME/$VIDNAME-Scenes.csv Results/$VIDNAME-Scenes.csv
+done
 
 python3 hist.py Results Final
 python3 vid.py Final/images/summary.jpg
